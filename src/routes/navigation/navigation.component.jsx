d8699445e7798d56f6cb0f4ca28778ced822a243
Amdisan <amdisan1@gmail.com>
2022-11-15 12:58:27 +0200

    added toggle cart-list and cart context and styles

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,21 @@
 import { Fragment, useContext } from "react";
 import   { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import {ReactComponent as Logo} from "../../assets/crown.svg";
+
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils"; 
+
+import { CartContext } from "../../contexts/cart.context";
+
 import "./navigation.styles.scss";
 
 
 function Navigation() {
     const {currentUser} = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext);  
         
 
     return (
@@ -17,14 +25,17 @@ function Navigation() {
             <Logo className="logo"/>
           </Link>            
           <div className="nav-links-container">
-            <Link className="nav-link" to= "/shop"> SHOP </Link>
+            <Link className="nav-link" to= "/shop"> SHOP
+             </Link>
             {currentUser ? (
               <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
             ) : (
               <Link className="nav-link" to= "/authentication"> SIGN IN </Link>
             )}
+            <CartIcon />
             
           </div>
+          {isCartOpen && <CartDropdown/>}
         </div>
         <Outlet/>
       </Fragment>    
